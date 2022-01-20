7780fa77a3ec067354d90ffddc84d99abda118a5
nix.arif <nix.arif@gmail.com>
2022-01-20 14:22:20 +0800

    Login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,22 @@
-const Login = () => <h1>TODO</h1>;
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
+
+const Login = () => {
+	const navigate = useNavigate();
+	const { login } = useAuth();
+
+	const handleLogin = () => {
+		login().then(() => {
+			navigate('/dashboard');
+		});
+	};
+
+	return (
+		<div>
+			<h1>login</h1>
+			<button onClick={handleLogin}>Log in</button>
+		</div>
+	);
+};
 
 export default Login;
