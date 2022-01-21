860c5a8c51c50dfe2698db2374181b0094cca8e3
nix-arif <nix.arif@gmail.com>
2022-01-21 22:41:52 +0800

    Browser router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import './App.css';
-import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Pricing from './components/Pricing';
-import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Setting from './components/Setting';
+import "./App.css";
+import React from "react";
+import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Pricing from "./components/Pricing";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Setting from "./components/Setting";
 
 function Nav() {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/pricing">Pricing</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Pricing</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 function App() {
-	return (
-		<div className="App">
-			<div>
-				<Nav />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/pricing" element={<Pricing />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route path="/setting" element={<Setting />} />
-					<Route path="/login" element={<Login />} />
-				</Routes>
-			</div>
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
